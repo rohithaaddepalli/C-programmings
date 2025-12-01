71c5d539aefd963d3ca92d041d484513dc30a26f
rohithaaddepalli <rohithaaddepalli95@gmail.com>
2025-12-01 11:02:32 +0530

    Add files via upload

---
+++ sub of 2no. using functions.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void sub(int,int);
+int main()
+{
+	int a,b;
+	printf("enter a&b values");
+	scanf("%d%d",&a,&b);
+	sub(a,b);
+	return 0;
+}
+void sub(int a,int b)
+{
+	int c;
+	c=a-b;
+	printf("sub=%d",c);
+}
