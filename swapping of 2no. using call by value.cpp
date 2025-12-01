c0fb2b21b2968e31b70081ca3ad6df067319c1f1
rohithaaddepalli <rohithaaddepalli95@gmail.com>
2025-12-01 11:05:26 +0530

    Add files via upload

---
+++ swapping of 2no. using call by value.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void swap(int ,int);
+int main()
+{
+	int a,b;
+	printf("enter a,b values");
+	scanf("%d%d",&a,&b);
+	printf("before swapping a=%d,b=%d",a,b);
+	swap(a,b);
+	return 0;
+}
+void swap(int a,int b)
+{
+	int temp;
+	temp=a;
+	a=b;
+	b=temp;
+	printf("\nafter swapping a=%d b=%d",a,b);
+}
